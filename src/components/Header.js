196c4bf1f19c40a8673e0c460541101f01613023
James Fox <jimmyfox00@gmail.com>
2022-07-01 09:51:02 -0700

    Added header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div style={{textAlign: "center"}}>
+            <h1>Tap Room</h1>
+            <h4>Keep Track of all your drinks!</h4>
+        </div>
+    )
+}
+
+export default Header;
